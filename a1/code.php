0ce41c047313a0f71f2efc5cb3642f859fa04ded
Faye <fayebheevayne@gmail.com>
2022-03-10 20:56:03 +0800

    updated activity code

--- a1/code.php
+++ a1/code.php
@@ -10,6 +10,36 @@ class Building {
 		$this->floors = $floors;
 		$this->address = $address;
 	}
+
+		// Encapsulation - getter
+	public function getName(){
+		return $this->name;
+	}
+
+		// Encapsulation - setter
+	public function setName($name){
+		$this->name = $name;
+	}
+
+		// Floors - getter
+	public function getFloors(){
+		return $this->floors;
+	}
+
+		// Floors - setter
+	public function setFloors($floors){
+		$this->floors = $floors;
+	}
+
+		// Address - getter
+	public function getAddress(){
+		return $this->address;
+	}
+
+		// Address - setter
+	public function setAddress($address){
+		$this->address = $address;
+}
 }
 
 class Condominium extends Building{
--- a1/index.php
+++ a1/index.php
@@ -9,17 +9,19 @@
 </head>
 <body>
 		<h1>Building</h1>
-		<p>The name of this building is <?php echo $building->name; ?></p>
-		<p>The Caswynn Building has <?php echo $building->floors; ?> floors</p>
-		<p>The Caswynn Building is <?php echo $building->address; ?></p>
+		<p>The name of this building is <?php echo $building->getName(); ?></p>
+		<p>The Caswynn Building has <?php echo $building->getFloors(); ?> floors</p>
+		<p>The Caswynn Building is <?php echo $building->getAddress(); ?></p>
+		<?php $building->setName('Caswynn Complex') ?>
+		<p>The name of the building has been changed to <?php echo $building->getName(); ?></p>
 		
 
 
 
 		<h1>Condominium</h1>
-		 <p>The name of this condominium is <?php echo $condominium->name; ?></p>
-		<p>The Enzo Condo has <?php echo $condominium->floors; ?> floors</p>
-		<p>The Enzo Condo is <?php echo $condominium->address; ?></p>
+		 <p>The name of this condominium is <?php echo $condominium->getName(); ?></p>
+		<p>The Enzo Condo has <?php echo $condominium->getFloors(); ?> floors</p>
+		<p>The Enzo Condo is <?php echo $condominium->getAddress(); ?></p>
 
 		<?php $condominium->setName('Enzo Tower') ?>
 		<p>The name of the condominium has been changed to <?php echo $condominium->getName(); ?></p>
